5673d8d54f9d84b8f0b9920665760f9b3764b26e
skywalle <125543@qq.com>
2017-09-26 01:01:10 +0800

    初始化

---
+++ src/main/java/com/chaoshu/jingdong/JingdongApplication.java
@@ -0,0 +1,12 @@
+package com.chaoshu.jingdong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JingdongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JingdongApplication.class, args);
+	}
+}
